979d404672e18c1f8fa7deadbb43ed51b09238ee
JuansebastianReyes <reyes_garcia1103@hotmail.com>
2017-10-05 18:19:05 -0500

    Se añade funcion nieto

--- Prolog/familias.pl
+++ Prolog/familias.pl
@@ -35,17 +35,32 @@ progenitor(camila,luis).
 progenitor(camila,manuel).
 
 pareja(X,Y) :- progenitor(X,Z),progenitor(Y,Z),X \== Y.
-
-adoptado(A) :- progenitor(X,A),hombre(X),hombre(Y),pareja(X,Y).
-
-abuelo(X,Y) :- progenitor(X,Z) , progenitor(Z,Y).
-
-bisabuelo(X,Y) :- progenitor(X,Z), abuelo(Z,Y).
-
-
-
 padre(X,Y):- hombre(X),progenitor(X,Y).
 madre(X,Y):- mujer(X),progenitor(X,Y).
+adoptado(A) :- progenitor(X,A),hombre(X),hombre(Y),pareja(X,Y).
+abuelo(X,Y) :- progenitor(X,Z) , progenitor(Z,Y).
+bisabuelo(X,Y) :- progenitor(X,Z), abuelo(Z,Y).
+nieto(X,Y) :- progenitor(Y,Z),progenitor(Z,X).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
